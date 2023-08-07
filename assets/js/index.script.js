bbb8947aab75fe40a9fd6dee527163e814cf927a
vilasboasfabio <vilasboasfabio564@gmail.com>
2023-08-07 11:20:28 -0300

    feat: creat savepost and storepost functions

---
+++ assets/js/index.script.js
@@ -0,0 +1,26 @@
+const post = [];
+
+function savePost(){
+    const title = document.getElementById('title').value;
+    const resume = document.getElementById('resume').value;
+    const publisher = document.getElementById('publisher').value;
+    const date = document.getElementById('date').value;
+
+    if(title && resume && publisher && date){
+        storePost(title, resume, publisher, date);
+     
+
+}
+
+
+}
+function storePost(title, resume, publisher, date){
+    const post = {
+        title,
+        resume,
+        publisher,
+        date
+    };
+
+    posts.push(post);
+}
